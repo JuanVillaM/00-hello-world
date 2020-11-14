ab300194b0c887ce51bdf8a349969bfcf19cc913
Juan Villa <juanestebanvillamemdoza@gmail.com>
2020-11-14 16:48:48 -0400

    Agregando archivo app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+console.log('Hello World Juan!');
+
+let c;
+let f = 70;
+let r = 32;
+let d = 5 / 9;
+
+console.log(`${f} grados farenheit son ${(f - r) * (d)} grados celsius.`);
+
+let edad = 16;
+let activo = true;
+let nombre = 'Juan';
+let ocupacion = 'Excelente Programador';
+
+if (activo) {
+    console.log(`${nombre} tiene ${edad} años y es un ${ocupacion}.`);
+} else {
+    console.log('Juan no está activo.');
+}
